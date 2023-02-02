8d39652c4565005fe902685e5a9ab47910404e35
Amarpreet Singh <solamarpreet@gmail.com>
2023-02-02 18:11:13 +0530

    Terraform and Ansible manifests uploaded

---
+++ terraform/main.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+
+provider "aws" {
+  shared_config_files      = ["$HOME/.aws/config"]
+  shared_credentials_files = ["$HOME/.aws/credentials"]
+  profile                  = var.profile
+}
---
+++ terraform/terraform.tfvars
@@ -0,0 +1,3 @@
+ssh_public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJGW9bJXPkq7xKlQ64wA+Jx071k8hl7vwLV01Ew90SWC sol@sol-PC"
+ansible_ssh_private_key_file = "/home/sol/.ssh/badal@aws"
+profile = "default"
